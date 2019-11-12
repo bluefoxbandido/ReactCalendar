bba718c0a758934a94cc2677d5b90cdb6f1b3045
bluefoxbandido <adamthewonderboy@gmail.com>
2019-11-12 14:10:31 -0600

    Dynamically Rendered Blocks

--- src/components/app.js
+++ src/components/app.js
@@ -4,16 +4,46 @@ import Weekday from './weekday';
 import DayBlock from './dayBlock';
 
 export default class App extends Component {
-  renderDays = () => {
-    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-    return days.map(day => <Weekday day={day} />)
+  constructor(props){
+    super (props)
+
+    this.days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+
+    this.state = {
+      month : "November",
+      year: "2019",
+      daysInMonth: 30,
+      daysInPreviousMonth: 31,
+      startDay: "Friday"
+    }
   }
+  renderDays = () => {
+    return this.days.map(day => <Weekday day={day} />)
+  }
+
+  renderBlocks = () => {
+    const blocks = [];
+    let fillerDate = (this.state.daysInPreviousMonth - this.days.indexOf(this.state.startDay)) + 1;
+
+    while (fillerDate <= this.state.daysInPreviousMonth){
+      blocks.push(<DayBlock date={fillerDate} />)
+      fillerDate++;
+    }
+
+    for (let i = 1; i <= this.state.daysInMonth; i++){
+      blocks.push(<DayBlock date={i} />)
+    }
+
+    return blocks
+  }
+
   render() {
     return (
       <div className='app'>
         <div className="header">
             <button>Previous Month</button>
-            <h1>November</h1>
+            <h1>{this.state.month}</h1>
             <button>Next Month</button>
         </div>
         <div className="calendar-wrapper">
@@ -21,11 +51,11 @@ export default class App extends Component {
                 {this.renderDays()}
             </div>
             <div className="blocks-wrapper">
-                <DayBlock date="27" />
+                {this.renderBlocks()}
             </div>
         </div>
         <div className="footer">
-            <h1>2019</h1>
+            <h1>{this.state.year}</h1>
         </div>
       </div>
     );
