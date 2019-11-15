5291f222531d9f0bae6a362c52c72bfb246dc5d3
bluefoxbandido <adamthewonderboy@gmail.com>
2019-11-14 22:40:41 -0600

    Added API

--- src/components/app.js
+++ src/components/app.js
@@ -1,46 +1,84 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-import Weekday from './weekday';
-import DayBlock from './dayBlock';
-
-import monthData from "../../static/assets/dummyData.json"
+import Weekday from './weekday'
+import DayBlock from './dayBlock'
 
 export default class App extends Component {
-  constructor(props){
-    super (props)
+  constructor(props) {
+    super(props)
 
     this.days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-    this.months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-    this.month = "November"
 
-    this.state = monthData[this.month]
+    this.state = {}
+    this.handleMonthChange = this.handleMonthChange.bind(this)
+  }
+
+  componentDidMount() {
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+    const today = new Date()
+    const date = today.getDate()
+    const month = months[today.getMonth()]
+    const year = today.getFullYear()
+
+    fetch(`http://127.0.0.1:5000/month/get/${month}/${year}`, {
+      method: "GET",
+      headers: { "Content-type": "application/json" }
+    })
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          month: data[1],
+          year: data[2],
+          daysInMonth: data[3],
+          daysInPreviousMonth: data[4],
+          startDay: data[5],
+          previousMonth: data[6],
+          nextMonth: data[7],
+          date: date,
+          trueMonth: month
+        })
+      })
+      .catch(error => {
+        console.log(error)
+      })
   }
 
   handleMonthChange(direction) {
-    let currentMonthIndex = this.months.indexOf(this.month)
-    
-    direction === "+" ? currentMonthIndex++ : currentMonthIndex--
-    
-    this.month = this.months[currentMonthIndex]
-
-    this.setState(monthData[this.month])
-
-    this.handleMonthChange = this.handleMonthChange.bind(this)
+    fetch(`http://127.0.0.1:5000/month/get/${direction === "+" ? this.state.nextMonth : this.state.previousMonth}/2019}`, {
+      method: "GET",
+      headers: { "Content-type": "application/json" }
+    })
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          month: data[1],
+          year: data[2],
+          daysInMonth: data[3],
+          daysInPreviousMonth: data[4],
+          startDay: data[5],
+          previousMonth: data[6],
+          nextMonth: data[7]
+        })
+      })
+      .catch(error => {
+        console.log(error)
+      })
   }
 
   renderDays = () => {
     return this.days.map(day => <Weekday day={day} />)
   }
-  
+
   renderBlocks = () => {
     const blocks = [];
     let fillerDate = (this.state.daysInPreviousMonth - this.days.indexOf(this.state.startDay)) + 1;
-    while (fillerDate <= this.state.daysInPreviousMonth){
-      blocks.push(<DayBlock date={fillerDate} filler={true}/>)
+    while (fillerDate <= this.state.daysInPreviousMonth) {
+      blocks.push(<DayBlock date={fillerDate} month={this.state.previousMonth} year={this.state.year} filler={true} />)
       fillerDate++;
     }
-    for (let i = 1; i <= this.state.daysInMonth; i++){
-      blocks.push(<DayBlock date={i} />)
+    for (let i = 1; i <= this.state.daysInMonth; i++) {
+      blocks.push(<DayBlock date={i} month={this.state.month} year={this.state.year} active={i == this.state.date && this.state.month == this.state.trueMonth ? true : false} />)
     }
     return blocks
   }
@@ -50,20 +88,20 @@ export default class App extends Component {
       <div className='app'>
         <div className="content-wrapper">
           <div className="header">
-              <button onClick={() => this.handleMonthChange("-")}>Previous Month</button>
-              <h1>{this.state.month}</h1>
-              <button onClick={() => this.handleMonthChange("+")}>Next Month</button>
+            <button onClick={() => this.handleMonthChange("-")}>Previous Month</button>
+            <h1>{this.state.month}</h1>
+            <button onClick={() => this.handleMonthChange("+")}>Next Month</button>
           </div>
           <div className="calendar-wrapper">
-              <div className="days-wrapper">
-                  {this.renderDays()}
-              </div>
-              <div className="blocks-wrapper">
-                  {this.renderBlocks()}
-              </div>
+            <div className="days-wrapper">
+              {this.renderDays()}
+            </div>
+            <div className="blocks-wrapper">
+              {this.renderBlocks()}
+            </div>
           </div>
           <div className="footer">
-              <h1>{this.state.year}</h1>
+            <h1>{this.state.year}</h1>
           </div>
         </div>
       </div>
--- src/components/dayBlock.js
+++ src/components/dayBlock.js
@@ -5,15 +5,87 @@ export default class DayBlock extends Component {
         super(props);
 
         this.state = {
-
+            text: "",
+            noteExists: false
         } //End Constructor
+        this.handleChange = this.handleChange.bind(this)
+        this.handleLoseFocus = this.handleChange.bind(this)
+    }
+    componentDidMount() {
+        fetch(`http://127.0.0.1:5000/note/get/${this.props.date}/${this.props.month}/${this.props.year}`, {
+            headers: { "content-type": "application/json" }
+        })
+        .then(request => request.json())
+        .then(data => {
+            if (data) {
+                this.setState({
+                    text: data[0],
+                    noteExists: true
+                })
+            }
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }
 
+    handleChange = () => {
+        this.setState({
+            text: event.target.value
+        })
+    }
+    handleLoseFocus = () => {
+        if (!this.state.noteExists && this.state.text !== ""){
+            fetch(`http://127.0.0.1:5000/note/input/`, {
+                method: "POST",
+                headers: { "content-type": "application/json" },
+                body: JSON.stringify({
+                    day: this.props.date,
+                    month: this.props.month,
+                    year: this.props.year,
+                    text:this.state.text
+                })
+            })
+            .then(response => {
+                this.setState({
+                    noteExists: true
+                })
+            })
+            .catch(error => {
+                console.log(error)
+            })
+        } else if (this.state.noteExists && this.state.text !== ""){
+            fetch(`http://127.0.0.1:5000/note/update/${this.props.date}/${this.props.month}/${this.props.year}`, {
+                method: "PUT",
+                headers: { "content-type": "application/json" },
+                body: JSON.stringify({
+                    text: this.state.text
+                })
+            })
+            .catch(error => {
+                console.log(error)
+            })
+        } else if(this.state.noteExists && this.state.text === ""){
+            
+            fetch(`http://127.0.0.1:5000/note/get/${this.props.date}/${this.props.month}/${this.props.year}`, {
+                method: "DELETE",
+                headers: { "content-type": "application/json" }
+            })
+            .then(response => {
+                this.setState({
+                    noteExists: false
+                })
+            })
+            .catch(error => {
+                console.log(error)
+            })
+        }
     }
     render(){
         return (
-            <div className={`day-block${this.props.filler ? " filler" : ""}`}>
+            <div className={`day-block${this.props.filler ? " filler" : ""}`} style={{border: this.props.active ? "2px solid navy" : "none"}}>
                 <div className="date">{this.props.date}</div>
-                <textarea className="calendar-input" />
+                <textarea className="calendar-input" onChange={this.handleChange} onBlur={this.handleLoseFocus} value={this.state.text} disabled={this.props.filler ? true : false} />
             </div>
         )
     }
